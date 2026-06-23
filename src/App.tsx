a70e34272e16906b55d638ce8c3cf1d1bebacd3f
nicksname1 <252460267+nicksname1@users.noreply.github.com>
2026-06-23 00:48:27 -0400

    feat: attach twitch icon for messages from twitch

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css"
 import "./preflight.css"
 import tmi from "tmi.js"
 type Chat = {
+	source: "twitch" | "youtube"
 	key: string | null
 	author: string
 	message: string
@@ -20,7 +21,8 @@ function App() {
 	client.connect()
 
 	client.on("message", (_channel, tags, message, _self) => {
-		const chat = {
+		const chat: Chat = {
+			source: "twitch",
 			key: tags.id || null,
 			author: `${tags["display-name"]}`,
 			message: message,
@@ -43,15 +45,40 @@ function App() {
 				{chat.map((i, index) => {
 					return (
 						<div key={index}>
-							<span
-								style={{
-									color: i.color,
-									fontWeight: "bold",
-								}}>
-								{i.author}:
-							</span>{" "}
-							&nbsp;
-							{i.message}
+							<span>
+								{i.source === "twitch" ? (
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width={16}
+										height={16}
+										viewBox="0 0 256 256">
+										<path
+											fill="#b446ed"
+											d="M208 32H48a16 16 0 0 0-16 16v144a16 16 0 0 0 16 16h16v32a8 8 0 0 0 13.12 6.15L122.9 208h42.2a16 16 0 0 0 10.25-3.71l42.89-35.75a15.93 15.93 0 0 0 5.76-12.29V48a16 16 0 0 0-16-16m-80 104a8 8 0 0 1-16 0V88a8 8 0 0 1 16 0Zm48 0a8 8 0 0 1-16 0V88a8 8 0 0 1 16 0Z"></path>
+									</svg>
+								) : (
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width={16}
+										height={16}
+										viewBox="0 0 256 256">
+										<path
+											fill="#ed4646"
+											d="M234.33 69.52a24 24 0 0 0-14.49-16.4C185.56 39.88 131 40 128 40s-57.56-.12-91.84 13.12a24 24 0 0 0-14.49 16.4C19.08 79.5 16 97.74 16 128s3.08 48.5 5.67 58.48a24 24 0 0 0 14.49 16.41C69 215.56 120.4 216 127.34 216h1.32c6.94 0 58.37-.44 91.18-13.11a24 24 0 0 0 14.49-16.41c2.59-10 5.67-28.22 5.67-58.48s-3.08-48.5-5.67-58.48m-73.74 65l-40 28A8 8 0 0 1 108 156v-56a8 8 0 0 1 12.59-6.55l40 28a8 8 0 0 1 0 13.1Z"></path>
+									</svg>
+								)}
+							</span>
+							<div>
+								<span
+									style={{
+										color: i.color,
+										fontWeight: "bold",
+									}}>
+									{i.author}
+								</span>
+								{": "}
+								<span>{i.message}</span>
+							</div>
 						</div>
 					)
 				})}
