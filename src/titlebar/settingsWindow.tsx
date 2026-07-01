0f2f0ccd1d24ac4540067a57f0fd349545425bd4
nicksname1 <252460267+nicksname1@users.noreply.github.com>
2026-07-01 09:41:45 -0400

    Style the settings form

--- src/titlebar/settingsWindow.tsx
+++ src/titlebar/settingsWindow.tsx
@@ -4,14 +4,18 @@ export const SettingsWindow = () => {
 	// const closeSelf = () => getCurrentWebviewWindow().close()
 
 	return (
-		<main>
+		<main id="settingsContainer">
 			{/* <button onClick={closeSelf}>Close</button> */}
-			<form action="">
-				<label htmlFor="twitchAccount">Twitch Channel</label>
-				<input
-					type="text"
-					id="twitchAccount"
-				/>
+			<form onSubmit={onSubmit}>
+				<div className="input">
+					<label htmlFor="twitchChannel">Twitch Channel</label>
+					<input
+						type="text"
+						id="twitchChannel"
+						name="twitchChannel"
+						defaultValue={twitchChannel}
+					/>
+				</div>
 				<button type="submit">Save</button>
 			</form>
 		</main>
